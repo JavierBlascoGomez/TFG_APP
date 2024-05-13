0babf510601ed108ed740fd96dcdc59ed41a8f7b
javier <alubla1033@ieselcaminas.org>
2024-05-13 09:43:49 +0200

    Initial commit

--- app/src/main/java/com/example/tfg_app/MainActivity.kt
+++ app/src/main/java/com/example/tfg_app/MainActivity.kt
@@ -1,33 +1,24 @@
 package com.example.tfg_app
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.tfg_app.ui.theme.TFG_APPTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TFG_APPTheme {
+                Scaffold {
 
+                }
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TFG_APPTheme {
     }
 }
---
+++ app/src/main/java/com/example/tfg_app/data/mappers/a.kt
@@ -0,0 +1,2 @@
+package com.example.tfg_app.data.mappers
+
